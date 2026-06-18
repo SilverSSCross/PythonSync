2a956e76dac5cd2bb52f8b869221506fe27d3703
Silver <crosssanslv@gmail.com>
2026-06-18 15:38:35 +0200

    18/06/2026

--- src/test_pcremote.py
+++ src/test_pcremote.py
@@ -1,4 +1,7 @@
 from core import pctoremote
+from core import remotetopc
 from config import ini_checker
 
-pctoremote.pctoremote_function(ini_checker.find_iniconfig())
+#pctoremote.pctoremote_function(ini_checker.find_iniconfig())
+
+remotetopc.remotetopc_funciton(ini_checker.find_iniconfig())
